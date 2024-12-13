afc1a0513d1e594a5b730e796c58b600fb3b7c9d
ThanhDatThanhDat <dt2229992@gmail.com>
2024-12-13 20:59:09 +0700

    ss16-2

---
+++ ss16-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void swapValues(int* a, int* b) {
+    int temp = *a;  
+    *a = *b;        
+    *b = temp;      
+}
+
+int main() {
+    int x = 5, y = 10;  
+    printf("Truoc khi doi: x = %d, y = %d\n", x, y);
+    swapValues(&x, &y);  
+    printf("Sau khi doi: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
